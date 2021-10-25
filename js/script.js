3f574a52e32f7a9de1c4d37574896e2d1c07495e
Doken1331 <lass245@mail.ru>
2021-10-25 23:20:23 +0300

    changed modul window

--- js/script.js
+++ js/script.js
@@ -1,25 +1,28 @@
 let cardModul = document.querySelector('.popup');
+let modulImg = document.querySelector('.popup__img');
 let modulTitle = document.querySelector('.popup__order-title');
 let modulPrice = document.querySelector('.popup__order-price');
 let buttonCard = document.querySelectorAll('.catalog__card-button');
 let cardTitle = document.querySelectorAll('.catalog__card-title');
 let cardPrice = document.querySelectorAll('.catalog__card-price');
+let cardImg = document.querySelectorAll('.catalog__card-pic');
 
 
-// buttonCard.addEventListener("click", () => {
-//     cardModul.style.display = "block";
-//     console.log(cardTitle);
-// })
+// buttonCard.forEach(element => {
+//     element.addEventListener("click", () => {
+//         cardModul.style.display = "block";
+//         modulImg.innerHTML = element.parentElement.querySelector('.catalog__card-pic').innerHTML;
+//         modulTitle.innerHTML = element.parentElement.querySelector('.catalog__card-title').innerHTML;
+//         modulPrice.innerHTML = element.parentElement.querySelector('.catalog__card-price').innerHTML;
+//     })
+// });
 
 
-buttonCard.forEach(element => {
-    element.addEventListener("click", () => {
+for (let i = 0; i < buttonCard.length; i++) {
+    buttonCard[i].onclick = function() {
         cardModul.style.display = "block";
-        console.log(element);
-        modulTitle.innerHTML = element.parentElement.querySelector('.catalog__card-title').innerHTML;
-        modulPrice.innerHTML = element.parentElement.querySelector('.catalog__card-price').innerHTML;
-    })
-});
-
-
-console.log(buttonCard);
+        modulImg.innerHTML = buttonCard[i].parentElement.querySelector('.catalog__card-pic').innerHTML;
+        modulTitle.innerHTML = buttonCard[i].parentElement.querySelector('.catalog__card-title').innerHTML;
+        modulPrice.innerHTML = buttonCard[i].parentElement.querySelector('.catalog__card-price').innerHTML;
+    }
+};
