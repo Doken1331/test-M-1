4ef6d7f01e96180c675b05c04ad3629701fc5c09
Doken1331 <lass245@mail.ru>
2021-10-26 03:27:18 +0300

    changed modul window

--- js/script.js
+++ js/script.js
@@ -3,20 +3,9 @@ let modulImg = document.querySelector('.popup__img');
 let modulTitle = document.querySelector('.popup__order-title');
 let modulPrice = document.querySelector('.popup__order-price');
 let buttonCard = document.querySelectorAll('.catalog__card-button');
-let cardTitle = document.querySelectorAll('.catalog__card-title');
-let cardPrice = document.querySelectorAll('.catalog__card-price');
-let cardImg = document.querySelectorAll('.catalog__card-pic');
-
-
-// buttonCard.forEach(element => {
-//     element.addEventListener("click", () => {
-//         cardModul.style.display = "block";
-//         modulImg.innerHTML = element.parentElement.querySelector('.catalog__card-pic').innerHTML;
-//         modulTitle.innerHTML = element.parentElement.querySelector('.catalog__card-title').innerHTML;
-//         modulPrice.innerHTML = element.parentElement.querySelector('.catalog__card-price').innerHTML;
-//     })
-// });
-
+let activeCatalog = document.getElementsByClassName('catalog__buttons-btn');
+let btnOne = document.getElementById('btn-active-1');
+let btnTwo = document.getElementById('btn-active-2');
 
 for (let i = 0; i < buttonCard.length; i++) {
     buttonCard[i].onclick = function() {
@@ -24,5 +13,21 @@ for (let i = 0; i < buttonCard.length; i++) {
         modulImg.innerHTML = buttonCard[i].parentElement.querySelector('.catalog__card-pic').innerHTML;
         modulTitle.innerHTML = buttonCard[i].parentElement.querySelector('.catalog__card-title').innerHTML;
         modulPrice.innerHTML = buttonCard[i].parentElement.querySelector('.catalog__card-price').innerHTML;
+        document.body.style.overflow = 'hidden';
+        document.querySelector('.catalog__card-pic_close').style.display = 'block';
+        document.querySelector('.catalog__card-pic_close').onclick = function() {
+            cardModul.style.display = "none";
+            document.body.style.overflow = '';
+        }
     }
 };
+
+btnOne.onclick = function() {
+    activeCatalog[0].classList.add('catalog__buttons-btn_active');
+    activeCatalog[1].classList.remove('catalog__buttons-btn_active');
+}
+
+btnTwo.onclick = function() {
+    activeCatalog[1].classList.add('catalog__buttons-btn_active');
+    activeCatalog[0].classList.remove('catalog__buttons-btn_active');
+}
